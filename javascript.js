20977fd95d43cf369daa2e79241e8c0b75501753
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-02-02 17:45:04 -0500

    Create functional RPS game

--- javascript.js
+++ javascript.js
@@ -1,32 +1,65 @@
-let computerChoice
-
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
-    switch (num) {
-        case 0:
-            computerChoice = "Rock";
-            break;
-        case 1:
-            computerChoice = "Paper";
-            break;
-        case 2:
-            computerChoice = "Scissors"
-            break;
-    }
-    return computerChoice
-}
-
-function getComputerChoices() {
-    let num = Math.floor(Math.random() * 3);
     if (num === 0) {
-        return "Rock";
+        return "rock";
     } else if (num === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
-// Chose one above this line
-function rpsRound(computerChoice, playerChoice) {
 
+function rpsRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        alert("It was a tie!");
+        return "tie";
+    } else if (computerChoice === "scissors" &&
+               playerChoice === "paper") {
+        alert("Computer wins! Scissors beats paper!");
+        return "lose";
+    } else if (computerChoice === "rock" &&
+               playerChoice === "scissors") {
+        alert("Computer wins! Rock beats scissors!");
+        return "lose";
+    } else if (computerChoice === "paper" &&
+               playerChoice === "rock") {
+        alert("Computer wins! Paper beats rock!");
+        return "lose";
+    } else if (computerChoice === "scissors" &&
+               playerChoice === "rock") {
+        alert("Player wins! Rock beats scissors!");
+        return "win";
+    } else if (computerChoice === "rock" &&
+               playerChoice === "paper") {
+        alert("Player wins! Paper beats rock!");
+        return "win";
+    } else {
+        alert("Player wins! Scissors beats paper!");
+        return "win";
+    }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("Rock! Paper! Scissors! Shoot").toLowerCase();
+        let roundScore = rpsRound(computerChoice, playerChoice);
+        if (roundScore === "lose") {
+            computerScore++;
+        } else if (roundScore === "win") {
+            playerScore++;
+        } else {
+        }
+    }
+    if (playerScore === computerScore) {
+        alert("Tie game!");
+    } else if (playerScore > computerScore) {
+        alert("Player wins!");
+    } else {
+        alert("Computer wins! Too bad!")
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
